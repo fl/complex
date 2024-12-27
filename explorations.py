ee3a2666198b750e7993808d25204a11d803bd80
Florenz <fl@well.com>
2024-12-27 13:10:07 +0100

    plot..

--- explorations.py
+++ explorations.py
@@ -1,5 +1,5 @@
+import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib as plt
 
 RESOLUTION = 99
 NITER = 20
@@ -9,12 +9,17 @@ imag = np.linspace(-2, 2, RESOLUTION)
 C = real + 1j*imag[:,None]
 Z = 0.*C
 Mask = np.ones_like(C, dtype=bool)
+Output = np.zeros_like(C, dtype=int)
 
 for i in range(NITER):
     Mask[2*np.sin(Z) > 2] = False
-    Z[Mask] = Z[Mask]**(2)
+    Z[Mask] = Z[Mask]**2
     Output += Mask
 Output = Output / NITER
 
 plt.figure(figsize=(8,8))
-plt.imshow(Output, cmap='gray')
+plt.yticks([])
+plt.xticks([])
+plt.title("Mandelbrot Experimente")
+plt.imshow(Output)
+plt.show()
