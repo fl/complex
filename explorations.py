e24daa7baaf0a05c7979eca70cd14fcffcf493be
Florenz <fl@well.com>
2024-12-27 14:10:00 +0100

    generate more frames

--- explorations.py
+++ explorations.py
@@ -1,25 +1,37 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-RESOLUTION = 500
-NITER = 15
+plt.style.use("default")
+plt.set_cmap("magma")
 
-real = np.linspace(-2, 2, RESOLUTION)
-imag = np.linspace(-2, 2, RESOLUTION)
-C = real + 1j*imag[:,None]
-Z = 0.*C
-Mask = np.ones_like(C, dtype=bool)
-Output = np.zeros_like(C, dtype=int)
+RESOLUTION = 20
+ITERATIONS = 15
 
-for i in range(NITER):
-    Mask[2*np.sin(Z) > 2] = False
-    Z[Mask] = Z[Mask]**5.35 + C[Mask]
-    Output += Mask
-Output = Output / NITER
+real = np.linspace(-2.5, 1.5, RESOLUTION)
+imag = np.linspace(-2.0, 2.0, RESOLUTION)
 
-plt.figure(figsize=(8,8))
-plt.yticks([])
-plt.xticks([])
-plt.title("Mandelbrot Experimente")
-plt.imshow(Output)
-plt.show()
+def iterate(t, c):
+    z = 0 * c
+    mask = np.ones_like(c, dtype=bool)
+    out = np.zeros_like(c, dtype=int)
+
+    for i in range(ITERATIONS):
+        mask[z**(i / ITERATIONS + 0.1) > 2.0] = False
+        z[mask] = z[mask]**2 + c[mask]
+        c *= np.exp(1j * np.pi / ITERATIONS /2)
+        c *= np.exp(2j * t * np.pi)
+        c *= 1.1
+        out += mask
+
+    out = out / ITERATIONS
+
+    return out
+
+t = np.linspace(0, 1, 10)
+for i in range(t.shape[0]):
+    c = real + 1j * imag[:, None]
+    c *= 0.5
+
+    out = iterate(t[i], c)
+    plt.figure(figsize=(5, 5))
+    plt.imsave(f"{i:03d}.png", out, cmap="magma")
