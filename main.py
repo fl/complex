1bca3929765ef35563d5d4e4d57226dcd388e60b
Florenz <fl@well.com>
2024-12-29 18:38:39 +0100

    initial state blinki

---
+++ main.py
@@ -0,0 +1,26 @@
+from pixels import * 
+import animations
+#import connect4
+
+
+print("\nWelcome to da blinkenlights 2 ")
+
+animations.welcome()
+
+
+
+#connect4.Game(2)
+
+#def f(x,y,f):
+#    if x %2 is 0:
+#        if y %2 is 0:
+#            return (0,f%15,0)
+#    return (f%15,0,0)
+#
+#def f2(x,y,f):
+#    if f%3 is 0:
+#        return (10,0,0)
+#    return (0,0,0)
+#
+#animCallback(f2, 60)
+
